9c1831fb423bd5981abe95a2cf9e236c89b011df
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-12-15 17:39:18 +0400

    add some changes

--- postman-task/server.js
+++ postman-task/server.js
@@ -4,11 +4,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const users = [
-  { name: "Rashad", phoneNumber: "304-719-6233" },
-  { name: "Niyaz", phoneNumber: "715-695-8959" },
-  { name: "Saleh", phoneNumber: "571-575-9370" },
-  { name: "Vusal", phoneNumber: "660-565-8401" },
-  { name: "Tamerlan", phoneNumber: "507-867-8404" },
+  { name: "Shabnam", phoneNumber: "654-567-6345" },
+  { name: "Naila", phoneNumber: "517-890-6758" },
+  { name: "Solmaz", phoneNumber: "231-755-9215" },
+  { name: "Shafag", phoneNumber: "605-565-3851" },
+  { name: "Sara", phoneNumber: "465-787-8564" },
 ];
 
 app.post("/phonebook", function (req, res) {
